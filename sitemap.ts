1a8d530dc61127d70959e37b3d1719271c049f1d
julienderjany-maker <julienderjany@gmail.com>
2026-05-25 22:01:21 +0100

    Delete sitemap.ts

--- sitemap.ts
+++
@@ -1,4 +0,0 @@
-import type { MetadataRoute } from "next";
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [{ url: "https://pioppi.cafe", lastModified: new Date(), priority: 1 }];
-}
